0dacbfe8dcb2bdb804b1b8069dac3178d6c96e88
Anthony Lu <lua@mit.edu>
2016-05-09 12:57:11 -0400

    Refactoring

--- hmm.py
+++ hmm.py
@@ -4,96 +4,93 @@ def categorical(p):
     return p.cumsum().searchsorted(np.random.uniform(0, p.sum()))
 
 class HMM(object):
-    def __init__(self, t, e):
-        self.t = np.asarray(t)
-        self.e = np.asarray(e)
+    def __init__(self, t, e, obs, start_state, end_state=None, states=None):
+        """An HMM that explains a sequence of observations.
 
-    def sample_states_exact(self, obs, start_state, end_state=None, out=None):
-        """Sample a state sequence conditioned on a sequence of observations.
-
+        t: K-by-K transition matrix
+        e: K-by-M emission matrix
         obs: a sequence of N observations, n = [1..N] (may contain Nones for missing data)
         start_state: the initial state, n = 0
         end_state: the ending state, n = N+1 (may be None to not condition on the ending state)
         out: a length-N vector to store the sampled states n = [1..N]
 
         """
+        self.t = np.asarray(t)
+        self.e = np.asarray(e)
+        self.obs = obs
+        self.start_state = start_state
+        self.end_state = end_state
         N = len(obs)
-        K, _ = self.e.shape
+        (K, M) = self.e.shape
+        assert (K, K) == self.t.shape
+        self.N = N
+        self.K = K
+        self.M = M
+        if states is None:
+            states = np.empty(N, dtype=int)
+        self.states = states
+
+    def sample_states_exact(self):
+        """Sample a state sequence using exact forward-backward sampling."""
+
+        N = self.N
+        K = self.K
 
         # forward filtering
         factors = np.empty((N, K))
-        factor = (start_state == np.arange(K))
+        factor = (self.start_state == np.arange(K))
         for n in range(N):
             factor = np.dot(factor, self.t)
-            if obs[n] is not None:
-                factor *= self.e[:, obs[n]]
+            if self.obs[n] is not None:
+                factor *= self.e[:, self.obs[n]]
             factor /= np.sum(factor)
             factors[n] = factor
 
         # backward sampling
-        if out is None:
-            out = np.empty(N, dtype=int)
-        state = end_state
+        state = self.end_state
         for n in reversed(range(N)):
             factor = factors[n]
             if state is not None:
                 factor = factor * self.t[:, state]
             state = categorical(factor)
-            out[n] = state
+            self.states[n] = state
 
-        return out
+    def sample_states_slice(self, iters=1):
+        """Sample a state sequence using slice (beam) sampling."""
 
-    def sample_states_slice(self, obs, start_state, states, end_state=None, iters=1):
         for _ in range(iters):
-            slices = self.sample_slices(states, start_state)
-            self.sample_states_given_slices(obs, slices, start_state, end_state=end_state, out=states)
-        return states
+            slices = self.sample_slices()
+            self.sample_states_given_slices(slices)
 
-    def sample_slices(self, states, start_state):
-        """Sample the slice variables needed for sample_states_slice.
+    def sample_slices(self):
+        """Sample the slice variables needed for sample_states_slice."""
 
-        states: the sequence of N states, n = [1..N] on which depend
-        the conditional distribution of the slice variables
-        start_state: the initial state, n = 0
+        prev_states = np.concatenate(([self.start_state], self.states[:-1]))
+        return np.random.uniform(0, self.t[prev_states, self.states])
 
-        """
-        prev_states = np.concatenate(([start_state], states[:-1]))
-        return np.random.uniform(0, self.t[prev_states, states])
+    def sample_states_given_slices(self, slices):
+        """Sample a state sequence conditioned on a sequence of slice variables to limit the pool of states."""
 
-    def sample_states_given_slices(self, obs, slices, start_state, end_state=None, out=None):
-        """Sample a state sequence conditioned on a sequence of observations, using slice sampling to limit the pool of states.
-
-        obs: a sequence of N observations, n = [1..N] (may contain Nones for missing data)
-        slices: a sequence of N u-values, the auxiliary slice variables used to threshold the set of considered states
-        start_state: the initial state, n = 0
-        end_state: the ending state, n = N+1 (may be None to not condition on the ending state)
-        out: a length-N vector to store the sampled states n = [1..N]
-
-        """
-        N = len(obs)
-        K, _ = self.e.shape
+        N = self.N
+        K = self.K
 
         # forward filtering
         factors = np.empty((N, K))
-        factor = (start_state == np.arange(K))
+        factor = (self.start_state == np.arange(K))
         for n in range(N):
             factor = np.dot(factor, slices[n] < self.t)
-            if obs[n] is not None:
-                factor = factor * self.e[:, obs[n]]
+            if self.obs[n] is not None:
+                factor = factor * self.e[:, self.obs[n]]
             factor /= np.sum(factor)
             factors[n] = factor
 
         # backward sampling
-        if out is None:
-            out = np.empty(N)
-        state = end_state
+        state = self.end_state
         for n in reversed(range(N)):
             factor = factors[n]
-            if state is not end_state:
+            if state is not self.end_state:
                 factor = factor * (slices[n+1] < self.t[:, state])
             elif state is not None:
                 factor = factor * self.t[:, state]
             state = categorical(factor)
-            out[n] = state
-
-        return out
+            self.states[n] = state
--- test.py
+++ test.py
@@ -13,14 +13,15 @@ def basic_hmm():
     e = [[0.9, 0.1],
          [0.2, 0.8],
          [0, 0]]
-    h = HMM(t, e)
     obs = [0, 0, 1, 0, 0]
-    return (h, obs)
+    return HMM(t, e, obs, start_state=2)
 
 def test_sample_states_exact(basic_hmm, n_samples=1000):
-    (h, obs) = basic_hmm
-    samples = [h.sample_states_exact(obs, start_state=2)
-               for _ in range(n_samples)]
+    h = basic_hmm
+    samples = []
+    for _ in range(n_samples):
+        h.sample_states_exact()
+        samples.append(np.array(h.states))
     assert all(s in [0, 1] for sample in samples for s in sample)
     check_marginals(samples, [0.1327, 0.1796, 0.6925, 0.1796, 0.1327])
 
@@ -36,10 +37,11 @@ def check_marginals(samples, marginals):
     assert all(p > 0.01/len(ps) for p in ps)
 
 def test_sample_states_slice(basic_hmm, n_samples=1000):
-    (h, obs) = basic_hmm
-    states = h.sample_states_exact(obs, start_state=2)
-    samples = [np.array(h.sample_states_slice(obs, start_state=2,
-                                              states=states, iters=1))
-               for _ in range(n_samples)]
+    h = basic_hmm
+    h.sample_states_exact()
+    samples = []
+    for _ in range(n_samples):
+        h.sample_states_slice()
+        samples.append(np.array(h.states))
     assert all(s in [0, 1] for sample in samples for s in sample)
     check_marginals(samples, [0.1327, 0.1796, 0.6925, 0.1796, 0.1327])
